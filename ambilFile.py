470c95abbd555f22a0d4703cc7c0bc07f919f0a1
emotment <emotment@gmail.com>
2018-04-09 16:21:35 +0700

    make it better

--- ambilFile.py
+++ ambilFile.py
@@ -5,14 +5,14 @@ Created on Sun Apr  8 13:54:01 2018
 @author: emotment
 """
 import requests
-
+'''
 ## INI UNTUK KIRIM FILE ##
 r = requests.post('http://127.0.0.1:5000/todo/lone/', 
                   files={'file': open('aaa.pdf', 'rb')})
 '''
 ## INI UNTUK AMBIL FILE ##
-r = requests.get('http://127.0.0.1:5000/uploads/dompo.jpg/12345')
+r = requests.get('http://127.0.0.1:5000/uploads/asertipm.pdf/12345')
 s = r.content
-fo = open('gaga.jpg','wb')
+fo = open('gaga.pdf','wb')
 fo.write(s)
-fo.close()'''
+fo.close()
